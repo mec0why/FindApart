76eceb322c6be5df1b5633af770ac28da3d3d5fe
mec0why <calik127@gmail.com>
2024-12-09 20:48:46 +0100

    Migrate to ViewBinding

--- app/src/main/java/com/wsb/findapart/ui/home/HomeFragment.kt
+++ app/src/main/java/com/wsb/findapart/ui/home/HomeFragment.kt
@@ -2,23 +2,32 @@ package com.wsb.findapart.ui.home
 
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
 import com.wsb.findapart.R
+import com.wsb.findapart.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
     private lateinit var viewModel: HomeViewModel
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val textView = view.findViewById<TextView>(R.id.text_home)
+        _binding = FragmentHomeBinding.bind(view)
+
+        val textView = binding.textHome
 
         viewModel = ViewModelProvider(this).get<HomeViewModel>()
         viewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/wsb/findapart/ui/map/MapFragment.kt
+++ app/src/main/java/com/wsb/findapart/ui/map/MapFragment.kt
@@ -2,23 +2,32 @@ package com.wsb.findapart.ui.map
 
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
 import com.wsb.findapart.R
+import com.wsb.findapart.databinding.FragmentMapBinding
 
 class MapFragment : Fragment(R.layout.fragment_map) {
     private lateinit var viewModel: MapViewModel
+    private var _binding: FragmentMapBinding? = null
+    private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val textView = view.findViewById<TextView>(R.id.text_map)
+        _binding = FragmentMapBinding.bind(view)
+
+        val textView = binding.textMap
 
         viewModel = ViewModelProvider(this).get<MapViewModel>()
         viewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
